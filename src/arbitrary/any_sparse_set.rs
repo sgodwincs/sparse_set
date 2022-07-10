c0de15e3c2614edb1764bf0895a0be9140829f33
Scott Godwin <sgodwincs@gmail.com>
2022-07-10 10:24:11 -0700

    Add Arbitrary impl for AnySparseSet (via a wrapper)

---
+++ src/arbitrary/any_sparse_set.rs
@@ -0,0 +1,75 @@
+//! `Arbitrary` implementations for the types in this crate.
+
+use std::{
+  marker::PhantomData,
+  ops::{Deref, DerefMut},
+};
+
+use arbitrary::{Arbitrary, Unstructured};
+
+use crate::{
+  any_sparse_set::{
+    any_value::AnyValueWrapper,
+    traits::{None, Trait},
+    AnySparseSet, SatisfyTraits,
+  },
+  SparseSetIndex,
+};
+
+/// Wrapper around an `AnySparseSet` to enable an `Arbitrary implementation.
+pub struct AnySparseSetWrapper<I, T, Traits: ?Sized + Trait = dyn None> {
+  set: AnySparseSet<I, Traits>,
+  _marker: PhantomData<T>,
+}
+
+impl<I, T, Traits: ?Sized + Trait> Deref for AnySparseSetWrapper<I, T, Traits> {
+  type Target = AnySparseSet<I, Traits>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.set
+  }
+}
+
+impl<I, T, Traits: ?Sized + Trait> DerefMut for AnySparseSetWrapper<I, T, Traits> {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.set
+  }
+}
+
+impl<'a, I: From<usize> + SparseSetIndex, T: 'static + Arbitrary<'a>, Traits: ?Sized + Trait>
+  Arbitrary<'a> for AnySparseSetWrapper<I, T, Traits>
+where
+  T: SatisfyTraits<Traits>,
+{
+  fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+    // Get the number of `T`s we should insert into our collection.
+    let len = u.arbitrary_len::<T>()?;
+    let mut set = AnySparseSet::<I, Traits>::with_capacity::<T>(len, len);
+
+    for _ in 0..len {
+      if *u.choose(&[false, true])? {
+        let index = u.int_in_range(0..=(len - 1))?.into();
+        let value = T::arbitrary(u)?;
+        set.insert(index, AnyValueWrapper::new(value));
+      }
+    }
+
+    Ok(AnySparseSetWrapper {
+      set,
+      _marker: PhantomData,
+    })
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_any_sparse_set_arbitrary() {
+    let bytes = (0..255).into_iter().collect::<Vec<u8>>().repeat(100);
+    let mut u = Unstructured::new(&*bytes);
+    let set: AnySparseSetWrapper<usize, usize> = AnySparseSetWrapper::arbitrary(&mut u).unwrap();
+    assert!(!set.is_empty());
+  }
+}
---
+++ src/arbitrary/mod.rs
@@ -0,0 +1,68 @@
+//! `Arbitrary` implementations for the types in this crate.
+
+#[cfg(feature = "any_vec")]
+mod any_sparse_set;
+
+use arbitrary::{Arbitrary, Unstructured};
+
+use crate::{SparseSet, SparseSetIndex, SparseVec};
+
+#[cfg(feature = "any_vec")]
+pub use any_sparse_set::*;
+
+impl<'a, I: From<usize> + SparseSetIndex, T: Arbitrary<'a>> Arbitrary<'a> for SparseSet<I, T> {
+  fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+    // Get the number of `T`s we should insert into our collection.
+    let len = u.arbitrary_len::<T>()?;
+    let mut set = SparseSet::with_capacity(len, len);
+
+    for _ in 0..len {
+      if *u.choose(&[false, true])? {
+        let index = u.int_in_range(0..=(len - 1))?.into();
+        let value = T::arbitrary(u)?;
+        set.insert(index, value);
+      }
+    }
+
+    Ok(set)
+  }
+}
+
+impl<'a, I: From<usize> + SparseSetIndex, T: Arbitrary<'a>> Arbitrary<'a> for SparseVec<I, T> {
+  fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+    // Get the number of `T`s we should insert into our collection.
+    let len = u.arbitrary_len::<T>()?;
+    let mut set = SparseVec::with_capacity(len);
+
+    for _ in 0..len {
+      if *u.choose(&[false, true])? {
+        let index = u.int_in_range(0..=(len - 1))?.into();
+        let value = T::arbitrary(u)?;
+        set.insert(index, value);
+      }
+    }
+
+    Ok(set)
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_sparse_set_arbitrary() {
+    let bytes = (0..255).into_iter().collect::<Vec<u8>>().repeat(100);
+    let mut u = Unstructured::new(&*bytes);
+    let set: SparseSet<usize, usize> = SparseSet::arbitrary(&mut u).unwrap();
+    assert!(!set.is_empty());
+  }
+
+  #[test]
+  fn test_sparse_vec_arbitrary() {
+    let bytes = (0..255).into_iter().collect::<Vec<u8>>().repeat(100);
+    let mut u = Unstructured::new(&*bytes);
+    let set: SparseVec<usize, usize> = SparseVec::arbitrary(&mut u).unwrap();
+    assert!(!set.is_empty());
+  }
+}
