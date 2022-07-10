97c55f115609ec984675e99e176f59a913f1b5b2
Scott Godwin <sgodwincs@gmail.com>
2022-07-10 08:40:49 -0700

    Add a type-erased SparseSet behind an unstable feature

---
+++ src/any_sparse_set/mod.rs
@@ -0,0 +1,13 @@
+//! A type-erased, sparsely populated set, written `AnySparseSet<I>`, where `I` is the index type.
+//!
+//! `I` must implement `SparseSetIndex`, in particular, it must be able to be converted to a `usize` index.
+//!
+//! See [this article](https://research.swtch.com/sparse) on more details behind the data structure.
+
+mod any_sparse_set;
+mod any_sparse_set_mut;
+mod any_sparse_set_ref;
+
+pub use any_sparse_set::*;
+pub use any_sparse_set_mut::*;
+pub use any_sparse_set_ref::*;
